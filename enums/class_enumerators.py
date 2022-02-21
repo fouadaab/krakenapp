259c99b652d1c2c9fd04ace5661e6d6ba8a95899
Fouad <fouad.aabid@gmail.com>
2022-02-21 18:19:35 +0100

    API test: return total spent in EUR

--- enums/class_enumerators.py
+++ enums/class_enumerators.py
@@ -2,4 +2,8 @@ import enum
 
 class PathNames(str, enum.Enum):
     KEYS_FOLDER = 'keys'
-    KEY = "api-key-1645110930539.key"
+    KEY = 'api-key-1645110930539.key'
+
+class Currency(str, enum.Enum):
+    EURO = 'EUR'
+    USDT = 'USDT'
--- kraken_api.py
+++ kraken_api.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
 from enums import class_enumerators
 from api import API
+from fastapi import FastAPI
+from typing import Optional
 import utils
 import pprint
 
-_sum = defaultdict(float)
-def _add(x):
-    _sum['cost'] += float(x)
-    return None
+spent = defaultdict(float)
+def str2float(val: str) -> float:
+    return float(val)
 
 # API key and secret location
 key_loc = utils.PathConstructor(class_enumerators.PathNames.KEYS_FOLDER.value, class_enumerators.PathNames.KEY.value)._str_path()
@@ -26,8 +27,23 @@ trades_history = api_object.query_private('TradesHistory', req_data)
 
 # process queried data
 trades = trades_history['result']['trades']
-trades_eur = {key:val for key,val in trades.items() if 'EUR' in val['pair'] and 'USDT' not in val['pair']}
+trades_eur = {
+    key:val for key,val in trades.items()
+    if class_enumerators.Currency.EURO in val['pair']
+    and class_enumerators.Currency.USDT not in val['pair']
+}
 # pprint.pprint(resp_eur)
 
-[_add(val['cost']) for val in trades_eur.values()]
-print(_sum.values())
+# Fill sum spent in EUR to hash table
+for val in trades_eur.values():
+    spent[class_enumerators.Currency.EURO] += str2float(val['cost'])
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Application": "Kraken's Custom Wallet Dashboard"}
+
+@app.get("/spent/{currency}")
+def read_item(currency: str):
+    return {"sum spent in": currency, "total": spent[currency]}
