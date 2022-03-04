81f895dff08c88d289b584b5e8e7029966befcfe
Fouad <fouad.aabid@gmail.com>
2022-03-04 18:19:27 +0100

    integrate basic dash app for testing

--- kraken_main.py
+++ kraken_main.py
@@ -1,18 +1,23 @@
 from collections import defaultdict
 from enums import class_enumerators
 from core.config import settings
-from db.db_client import MongoDB
 from apis.base import api_router
 from webapps.base import api_router as web_app_router
-#from app import create_app, app_conf
-from krakenapi.api_client import API
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from krakenapi.api_client import API as kraken_api
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 import utils
 import uvicorn
-import pprint
+from pathlib import Path
+import dash
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from dash import dcc, html
+from app.dashapp.layout import layout
+from app.dashapp.callbacks import register_callbacks
+from starlette.middleware.wsgi import WSGIMiddleware
+
+root_path = Path('.')
 
 spent = defaultdict(float)
 def str2float(val: str) -> float:
@@ -22,16 +27,16 @@ def str2float(val: str) -> float:
 key_loc = utils.PathConstructor(class_enumerators.PathNames.KEYS_FOLDER.value, class_enumerators.PathNames.KEY.value)._str_path()
 
 # Initialize API client
-api_object = API()
+krakenapi_client = kraken_api()
 
 # Read Kraken API key and secret stored in local file
-api_object.load_key(key_loc)
+krakenapi_client.load_key(key_loc)
 
 # prepare request
 req_data = {'trades': 'true'}
 
 # query servers
-trades_history = api_object.query_private('TradesHistory', req_data)
+trades_history = krakenapi_client.query_private('TradesHistory', req_data)
 
 # process queried data
 trades = trades_history['result']['trades']
@@ -46,16 +51,29 @@ for val in trades_eur.values():
     spent[class_enumerators.Currency.EURO] += str2float(val['cost'])
 
 
-# @app.get("/spent/{currency}")
-# def read_item(currency: str):
-#     return {"sum spent in": currency, "total": spent[currency]}
-
 def include_router(app):
     app.include_router(api_router)
     app.include_router(web_app_router)
-
+    
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
+    
+    # Meta tags for viewport responsiveness
+    meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
+
+    dashapp = dash.Dash(__name__,
+                         requests_pathname_prefix='/dash/',
+                         assets_folder= root_path / 'dashapp/assets',
+                         meta_tags=[meta_viewport],
+                         external_stylesheets=[dbc.themes.CERULEAN],
+			 prevent_initial_callbacks=False,
+			)
+
+    dashapp.title = 'Dashapp'    
+    dashapp.layout = layout
+    register_callbacks(dashapp)
+    
+    app.mount("/dash", WSGIMiddleware(dashapp.server))
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
@@ -65,8 +83,12 @@ def start_application():
 
 app = start_application()
 
+@app.get("/spent/{currency}")
+def read_item(currency: str):
+    return {"sum spent in": currency, "total": spent[currency]}
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     uvicorn.run("kraken_main:app", host='127.0.0.1', port=8000, reload=True)
 
     # server = create_app()
